51d47eb53e6f4195621c0bce43a13030b951c3a5
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-07-05 14:07:24 +0530

    20230705

--- c/bitwise/c-in-depth/14-1.c
+++ c/bitwise/c-in-depth/14-1.c
@@ -6,12 +6,47 @@ int main(void)
     int a, b;
     printf("Enter values of a and b in hexadecimal: ");
     scanf("%x%x", &a, &b);
+    putchar('\n');
 
     printf("a=%X\n", a);
+    printf("a=%d\n", a);
     displayBits(a);
+    putchar('\n');
 
-    printf("b=%x\n", b);
+    printf("b=%X\n", b);
+    printf("b=%d\n", b);
     displayBits(b);
+    putchar('\n');
 
     printf("a&b=%X\n", a&b);
+    printf("a&b=%d\n", a&b);
+    displayBits(a&b);
+    putchar('\n');
+
+    printf("a|b=%X\n", a|b);
+    printf("a|b=%d\n", a|b);
+    displayBits(a|b);
+    putchar('\n');
+
+    printf("a^b=%X\n", a^b);
+    printf("a^b=%d\n", a^b);
+    displayBits(a^b);
+    putchar('\n');
+
+    printf("~a=%X\n", ~a);
+    printf("~a=%d\n", ~a);
+    displayBits(~a);
+    putchar('\n');
+
+    printf("~b=%X\n", ~b);
+    printf("~b=%d\n", ~b);
+    displayBits(~b);
+    putchar('\n');
+
+    printf("~(~a)=%X\n", ~(~a));
+    printf("~(~a=)%d\n", ~(~a));
+    displayBits(~(~a));
+    putchar('\n');
+
+    return 0;
 }
--- c/bitwise/c-in-depth/common.c
+++ c/bitwise/c-in-depth/common.c
@@ -1,4 +1,12 @@
 void displayBits(int x)
 {
-
+    int i, mask;
+    for (i = 31; i >= 0; i--)
+    {
+        mask = 1 << i;
+        putchar((x & mask) ? '1' : '0');    /*Test and print ith bit*/
+        if(i%8 == 0)
+            putchar(' ');
+    }
+    putchar('\n');
 }
