36c991da27c2117c8242f080ad8d35b22995a093
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-07-11 14:03:49 +0530

    20230711

---
+++ java/dsa/src/main/java/local/ols2304/dsa/App.java
@@ -0,0 +1,13 @@
+package local.ols2304.dsa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
